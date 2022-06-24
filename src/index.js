39b8efa3c2e364751ed6c69ed2543c8c687930a7
Ronald <ronald.isael@hotmail>
2022-06-24 19:59:03 -0400

    se termino de conectar con la vista (solo con una)

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 //Esto es para importa el servidor express
 import  express  from "express";
-import ejs from "ejs";
-import {dirname} from "path";
+
+import {dirname, join} from "path";
 import { fileURLToPath } from "url";
 
 const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
-console.log(__dirname);
 
-app.set('views', )
-app.set('view engine', ejs);
+
+app.set('views', join(__dirname, "views") );
+app.set('view engine', 'ejs');
 
 app.get('/', (reg,res) => res.render('index'));
 
