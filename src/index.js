15125069340033d5e76eb9be3832948f88f99638
Ronald <ronald.isael@hotmail>
2022-06-24 19:46:57 -0400

    Se conecto con el servidor de express y la conexion con html mediante con la dependencia o modulo ejs y se esta ejecutando la ruta absoluta para conetar con las diferente ventanas o vistas

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log('Hello World!');
+//Esto es para importa el servidor express
+import  express  from "express";
+import ejs from "ejs";
+import {dirname} from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+console.log(__dirname);
+
+app.set('views', )
+app.set('view engine', ejs);
+
+app.get('/', (reg,res) => res.render('index'));
+
+app.listen(3000);
+
+console.log('Sever is Listing on Port: 3000');
